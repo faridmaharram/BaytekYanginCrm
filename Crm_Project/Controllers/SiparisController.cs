b0e1c1123442bcd58bd4e510c38f9e5f8ae090cb
Farid Maharramov <faridmaharramov@hotmail.com>
2017-08-19 02:35:49 +0300

    Siparis Tablosu olusturuldu ve Controller olusturuldu. Daha islem yapilmadi. Bazi sorular var

---
+++ Crm_Project/Controllers/SiparisController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace Crm_Project.Controllers
+{
+    public class SiparisController : Controller
+    {
+        private BytekCRMEntitiesMain db = new BytekCRMEntitiesMain();
+        private int UserId = WebSecurity.CurrentUserId;
+        // GET: TSiparis
+        public ActionResult Index()
+        {
+            return View(db.Teklifs.ToList());
+        }
+
+    }
+}
